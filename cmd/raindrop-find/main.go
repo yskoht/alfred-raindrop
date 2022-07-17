b5db6058894a80b28ea3ed18a50723ca260fc615
yskoht <ysk.oht@gmail.com>
2022-07-18 00:52:45 +0900

    remove aw from raindrop-find

--- cmd/raindrop-find/main.go
+++ cmd/raindrop-find/main.go
@@ -1,43 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	raindropFind "github.com/yskoht/alfred-raindrop/internal/raindrop-find"
 	"github.com/yskoht/alfred-raindrop/pkg/file"
 	"github.com/yskoht/alfred-raindrop/pkg/sqlite3"
-
-	aw "github.com/deanishe/awgo"
 )
-
-var wf *aw.Workflow
-
-func init() {
-	wf = aw.New()
-}
 
 func run() {
 	if !file.Exists(sqlite3.DB_FILE) {
-		wf.Warn("Database not found", "Please run `raindrop-sync`")
 		return
 	}
 
-	id := wf.Args()[0]
+	id := os.Args[1]
 
 	raindrop, err := raindropFind.RaindropFind(id)
 	if err != nil {
-		wf.FatalError(err)
 		return
 	}
 
-	wf.
-		NewItem(raindrop.Title).
-		Subtitle(raindrop.Link).
-		Arg(raindrop.Link).
-		Valid(true)
-
-	wf.WarnEmpty("Bookmark not found", "Try a different query?")
-	wf.SendFeedback()
+	fmt.Print(raindrop.Link)
 }
 
 func main() {
-	wf.Run(run)
+	run()
 }
